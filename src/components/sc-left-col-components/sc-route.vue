3dbab2586bee079d4a474609670f153c0ec92a0f
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-11 14:18:33 +0300

    add sc-route-component

---
+++ src/components/sc-left-col-components/sc-route.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="sc-route">
+    <step-title title="Маршрут" number="1" />
+  </div>
+</template>
+
+<script>
+import StepTitle from '@/components/sc-left-col-components/step-title.vue'
+
+export default {
+  components: {
+    StepTitle,
+  },
+}
+</script>
