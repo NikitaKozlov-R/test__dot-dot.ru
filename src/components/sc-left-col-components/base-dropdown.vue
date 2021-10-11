db27b2edf5faca3b888a9651d8dd9cc6f5575a08
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-11 20:44:52 +0300

    add v-model in base-dropdown

--- src/components/sc-left-col-components/base-dropdown.vue
+++ src/components/sc-left-col-components/base-dropdown.vue
@@ -1,6 +1,16 @@
 <template>
   <div class="base-dropdown">
-    <label class="" for="">{{ title }}</label>
+    <label class="base-dropdown__title" :for="title">{{ title }}</label>
+    <select :id="title" class="base-dropdown__list">
+      <option disabled selected hidden>{{ placeholder }}</option>
+      <option
+        v-for="item in options"
+        :key="item.id"
+        :value="item.item"
+        class="base-dropdown__list-item"
+        v-text="item.item"
+      ></option>
+    </select>
   </div>
 </template>
 
@@ -17,9 +27,30 @@ export default {
     },
     options: {
       required: true,
-      type: Object,
-      default: () => {},
+      type: Array,
+      default: () => [],
     },
   },
 }
 </script>
+
+<style>
+.base-dropdown {
+  width: 172px;
+  display: flex;
+  flex-direction: column;
+  margin-right: 12px;
+}
+.base-dropdown__title {
+  font-size: 2rem;
+  color: #fff;
+  letter-spacing: normal;
+  margin-bottom: 8px;
+}
+.base-dropdown__list {
+  width: 100%;
+  height: 44px;
+  font-size: 2rem;
+  padding: 0 16px;
+}
+</style>
--- src/components/sc-left-col-components/sc-transport.vue
+++ src/components/sc-left-col-components/sc-transport.vue
@@ -1,16 +1,29 @@
 <template>
   <step-title title="Транспорт" number="2" />
-  <base-dropdown
-    title="Грузоподъёмность"
-    placeholder="Не выбрано"
-    :options="[
-      { id: 1, item: 'Вариант 1' },
-      { id: 2, item: 'Вариант 2' },
-      { id: 3, item: 'Вариант 3' },
-      { id: 4, item: 'Вариант 4' },
-      { id: 5, item: 'Вариант 5' },
-    ]"
-  ></base-dropdown>
+  <div class="sc-transporn__container">
+    <base-dropdown
+      title="Грузоподъёмность"
+      placeholder="Не выбранa"
+      :options="[
+        { id: 1, item: 'Вариант 1' },
+        { id: 2, item: 'Вариант 2' },
+        { id: 3, item: 'Вариант 3' },
+        { id: 4, item: 'Вариант 4' },
+        { id: 5, item: 'Вариант 5' },
+      ]"
+    ></base-dropdown>
+    <base-dropdown
+      title="Способ погрузки"
+      placeholder="Не выбран"
+      :options="[
+        { id: 1, item: 'Вариант 1' },
+        { id: 2, item: 'Вариант 2' },
+        { id: 3, item: 'Вариант 3' },
+        { id: 4, item: 'Вариант 4' },
+        { id: 5, item: 'Вариант 5' },
+      ]"
+    ></base-dropdown>
+  </div>
 </template>
 
 <script>
@@ -24,3 +37,10 @@ export default {
   },
 }
 </script>
+
+<style>
+.sc-transporn__container {
+  width: 100%;
+  display: flex;
+}
+</style>
