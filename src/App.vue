6dd22313b237f72d293746ba68ab36c3fa1ef735
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-11 12:49:08 +0300

    add header-component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div class="wrapper">
+    <straight-car-header />
     <straight-car-left-col />
     <straight-car-right-col />
   </div>
 </template>
 
 <script>
+import StraightCarHeader from '@/components/straight-car-header.vue'
 import StraightCarLeftCol from '@/components/straight-car-left-col.vue'
 import StraightCarRightCol from '@/components/straight-car-right-col.vue'
 
 export default {
   name: 'App',
   components: {
+    StraightCarHeader,
     StraightCarLeftCol,
     StraightCarRightCol,
   },
@@ -21,7 +24,7 @@ export default {
 <style>
 .wrapper {
   width: 100%;
-  padding: 0 170px;
+  padding: 20px 170px;
   display: flex;
 }
 </style>
---
+++ src/components/straight-car-header.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1 class="sc-header">FTL: Прямая машина</h1>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+.sc-header {
+  font-weight: 700;
+  font-size: 7rem;
+  color: #fff;
+  padding: 20px 0;
+}
+</style>
