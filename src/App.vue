4504c2af0408fdade2c44f867733346fa928e6a1
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-11 12:38:35 +0300

    add 2 root components

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,27 @@
 <template>
-  <span></span>
+  <div class="wrapper">
+    <straight-car-left-col />
+    <straight-car-right-col />
+  </div>
 </template>
 
 <script>
+import StraightCarLeftCol from '@/components/straight-car-left-col.vue'
+import StraightCarRightCol from '@/components/straight-car-right-col.vue'
+
 export default {
   name: 'App',
+  components: {
+    StraightCarLeftCol,
+    StraightCarRightCol,
+  },
 }
 </script>
+
+<style>
+.wrapper {
+  width: 100%;
+  padding: 0 170px;
+  display: flex;
+}
+</style>
