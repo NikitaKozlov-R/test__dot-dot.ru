bc2c895d1895dd1ced8c7109f5d1db45cb8cca00
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-14 02:22:32 +0300

    remove components

--- src/components/straight-car-left-col.vue
+++ src/components/straight-car-left-col.vue
@@ -1,33 +1,8 @@
 <template>
   <div class="left-col">
-    <sc-header />
-    <sc-route />
-    <sc-transport />
-    <sc-cargo />
-    <sc-time />
-    <sc-comment />
+    <slot />
   </div>
 </template>
-
-<script>
-import ScHeader from '@/components/root-components/sc-header.vue'
-import ScRoute from '@/components/root-components/sc-route.vue'
-import ScTransport from '@/components/root-components/sc-transport.vue'
-import ScCargo from '@/components/root-components/sc-cargo.vue'
-import ScTime from '@/components/root-components/sc-time.vue'
-import ScComment from '@/components/root-components/sc-comment.vue'
-
-export default {
-  components: {
-    ScHeader,
-    ScRoute,
-    ScTransport,
-    ScCargo,
-    ScTime,
-    ScComment,
-  },
-}
-</script>
 
 <style>
 .left-col {
