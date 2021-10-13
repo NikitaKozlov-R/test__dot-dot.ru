35fc9a42975ce9d7d41538d83af9607174e5bfa1
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-13 15:23:01 +0300

    add base-radio-component at sc-time (root-component)

--- src/components/root-components/sc-time.vue
+++ src/components/root-components/sc-time.vue
@@ -1,8 +1,8 @@
 <template>
   <step-title title="Время" number="4" />
   <div class="sc-time">
-    <base-radio title="Когда прибыть на точку" title-right="700 км в день" />
-    <base-radio title="Когда прибыть на точку" title-right="700 км в день" />
+    <base-radio title="Когда прибыть на точку" title-right="700 км в день" unique-id="radio-1" />
+    <base-radio unique-id="radio-2" />
   </div>
 </template>
 
@@ -18,4 +18,9 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+.sc-time {
+  width: 100%;
+  margin-bottom: 16px;
+}
+</style>
