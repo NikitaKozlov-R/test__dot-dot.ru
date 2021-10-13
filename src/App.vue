59242ab0c2623f00999fef102077f699ed08d559
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-14 02:22:04 +0300

    add data at App.vue & components

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="wrapper">
-    <straight-car-left-col />
+    <straight-car-left-col>
+      <div class="left-col">
+        <sc-header />
+        <sc-route v-model:routeFrom="routeFrom" v-model:routeTo="routeTo" />
+        <sc-transport />
+        <sc-cargo />
+        <sc-time />
+        <sc-comment />
+      </div>
+    </straight-car-left-col>
     <straight-car-right-col />
   </div>
 </template>
@@ -8,12 +17,30 @@
 <script>
 import StraightCarLeftCol from '@/components/straight-car-left-col.vue'
 import StraightCarRightCol from '@/components/straight-car-right-col.vue'
+import ScHeader from '@/components/root-components/sc-header.vue'
+import ScRoute from '@/components/root-components/sc-route.vue'
+import ScTransport from '@/components/root-components/sc-transport.vue'
+import ScCargo from '@/components/root-components/sc-cargo.vue'
+import ScTime from '@/components/root-components/sc-time.vue'
+import ScComment from '@/components/root-components/sc-comment.vue'
 
 export default {
   name: 'App',
   components: {
     StraightCarLeftCol,
     StraightCarRightCol,
+    ScHeader,
+    ScRoute,
+    ScTransport,
+    ScCargo,
+    ScTime,
+    ScComment,
+  },
+  data() {
+    return {
+      routeFrom: '',
+      routeTo: '',
+    }
   },
 }
 </script>
