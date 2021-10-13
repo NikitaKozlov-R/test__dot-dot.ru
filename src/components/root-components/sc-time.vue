b94a90d815ffce038a26c71e6f35e40f0bbfa680
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-13 12:57:27 +0300

    start develop sc-time (root-component)

--- src/components/root-components/sc-time.vue
+++ src/components/root-components/sc-time.vue
@@ -1,9 +1,21 @@
 <template>
-  <div class="sc-time"></div>
+  <step-title title="Время" number="4" />
+  <div class="sc-time">
+    <base-radio title="Когда прибыть на точку" title-right="700 км в день" />
+    <base-radio title="Когда прибыть на точку" title-right="700 км в день" />
+  </div>
 </template>
 
 <script>
-export default {}
+import StepTitle from '@/components/base-components/step-title.vue'
+import BaseRadio from '@/components/base-components/base-radio.vue'
+
+export default {
+  components: {
+    StepTitle,
+    BaseRadio,
+  },
+}
 </script>
 
 <style></style>
