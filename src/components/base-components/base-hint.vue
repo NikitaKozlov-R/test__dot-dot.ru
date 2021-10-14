21c029443af32ca112c1c4e44e79223ed70914f6
Nikita Kozlov <kozlov_nikita@outlook.com>
2021-10-15 00:19:26 +0300

    complete base-hint component

--- src/components/base-components/base-hint.vue
+++ src/components/base-components/base-hint.vue
@@ -1,9 +1,36 @@
 <template>
-  <span></span>
+  <div class="base-hint">
+    <p class="base-hint__text">{{ text }}</p>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
+}
 </script>
 
-<style></style>
+<style>
+.base-hint {
+  width: 100%;
+  background-color: var(--COLOR-SUBSTRATE);
+  padding: 24px;
+  margin-top: 128px;
+}
+.base-hint__text {
+  font-size: 15px;
+  letter-spacing: normal;
+  line-height: 120%;
+  color: #fff;
+}
+@media screen and (max-width: 900px) {
+  .base-hint {
+    margin-top: 0;
+  }
+}
+</style>
